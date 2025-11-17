3179b058ee0c44632a8a2fe88b4c1b4516d635da
Mark Vella <me@markvella.dev>
2025-11-17 13:43:32 +0100

    fix: hurl config

--- lua/mrk/plugins/hurl.lua
+++ lua/mrk/plugins/hurl.lua
@@ -1,69 +1,86 @@
 return {
-        {
-                "jellydn/hurl.nvim",
-                dependencies = {
-                        "MunifTanjim/nui.nvim",
-                        "nvim-lua/plenary.nvim",
-                        "nvim-treesitter/nvim-treesitter",
+        "jellydn/hurl.nvim",
+        dependencies = {
+                "MunifTanjim/nui.nvim",
+                "nvim-lua/plenary.nvim",
+                "nvim-treesitter/nvim-treesitter",
+                {
+                        "MeanderingProgrammer/render-markdown.nvim",
+                        opts = { file_types = { "markdown" } },
+                        ft = { "markdown" },
                 },
-                ft = "hurl",
-                opts = {
-                        debug = false,
-                        show_notification = true,
-                        -- "split" or "popup"
-                        mode = "popup",
-                        formatters = {
-                                json = { "jq" },
-                        },
-                        mappings = {
-                                close = "q", -- Close popup/split
-                                next_panel = "<C-n>", -- Next response panel
-                                prev_panel = "<C-p>", -- Previous response panel
-                        },
-                        env_file = { "vars.env" },
+        },
+        ft = "hurl",
+        opts = {
+                debug = true,
+                show_notification = true,
+                mode = "popup",
+                formatters = {
+                        json = { "jq" },
+                        html = { "prettier", "--parser", "html" },
                 },
-                keys = {
+                mappings = {
+                        close = "q", -- Close popup/split
+                        next_panel = "<C-n>", -- Next response panel
+                        prev_panel = "<C-p>", -- Previous response panel
+                },
+                env_file = { "vars.env" },
+                fixture_vars = {
                         {
-                                "<leader>ha",
-                                "<cmd>HurlRunner<CR>",
-                                desc = "[h]url [a]ll in file",
+                                name = "random_int_number",
+                                callback = function()
+                                        return tostring(math.random(1, 1000))
+                                end,
                         },
                         {
-                                "<leader>hh",
-                                "<cmd>HurlRunnerAt<CR>",
-                                desc = "[h]url [h]ere",
+                                name = "random_float_number",
+                                callback = function()
+                                        return string.format("%.2f", math.random() * 10)
+                                end,
                         },
-                        {
-                                "<leader>he",
-                                "<cmd>HurlRunnerToEntry<CR>",
-                                desc = "[h]url run to [e]ntry",
-                        },
-                        {
-                                "<leader>hf",
-                                "<cmd>HurlRunnerToEnd<CR>",
-                                desc = "[h]url [f]ull",
-                        },
-                        {
-                                "<leader>ht",
-                                "<cmd>HurlToggleMode<CR>",
-                                desc = "[h]url [t]oggle",
-                        },
-                        {
-                                "<leader>hv",
-                                "<cmd>HurlVerbose<CR>",
-                                desc = "[h]url [v]erbose",
-                        },
-                        {
-                                "<leader>hV",
-                                "<cmd>HurlVeryVerbose<CR>",
-                                desc = "[h]url [V]ery verbose",
-                        },
-                        {
-                                "<leader>hs",
-                                "<cmd>HurlRunner<CR>",
-                                mode = "v",
-                                desc = "[h]url [s]election",
-                        },
+                },
+        },
+        keys = {
+                {
+                        "<leader>ha",
+                        "<cmd>HurlRunner<CR>",
+                        desc = "[h]url run [a]ll",
+                },
+                {
+                        "<leader>hh",
+                        "<cmd>HurlRunnerAt<CR>",
+                        desc = "[h]url run [h]ere",
+                },
+                {
+                        "<leader>hb",
+                        "<cmd>HurlRunnerToEntry<CR>",
+                        desc = "[h]url to entry",
+                },
+                {
+                        "<leader>hf",
+                        "<cmd>HurlRunnerToEnd<CR>",
+                        desc = "[h]url to end",
+                },
+                {
+                        "<leader>ht",
+                        "<cmd>HurlToggleMode<CR>",
+                        desc = "[h]url toggle",
+                },
+                {
+                        "<leader>hv",
+                        "<cmd>HurlVerbose<CR>",
+                        desc = "[h]url [v]erbose",
+                },
+                {
+                        "<leader>hV",
+                        "<cmd>HurlVeryVerbose<CR>",
+                        desc = "[h]url [V]ery verbose",
+                },
+                {
+                        "<leader>hs",
+                        "<cmd>HurlRunner<CR>",
+                        mode = "v",
+                        desc = "[h]url selection",
                 },
         },
 }
