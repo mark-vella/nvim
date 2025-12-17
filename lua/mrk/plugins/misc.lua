379efc875e2b992def8aec8aa4a29c122c3812dd
Mark Vella <me@markvella.dev>
2025-12-17 13:09:13 +0100

    feat: beacon and comfy lines

---
+++ lua/mrk/plugins/misc.lua
@@ -0,0 +1,11 @@
+return {
+        {
+                "mluders/comfy-line-numbers.nvim",
+                config = function()
+                        require("comfy-line-numbers").setup({})
+                end,
+        },
+        {
+                "danilamihailov/beacon.nvim",
+        },
+}
